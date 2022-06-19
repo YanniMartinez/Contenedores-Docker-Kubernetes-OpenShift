7e3226dc93005a486bafbddc4be5b1db6fba2a7f
Yann <metalyanni@gmail.com>
2022-06-19 17:09:00 -0500

    OpenShift Overview

---
+++ Semana3/Lab03/app.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var os = require("os");
+var hostname = os.hostname();
+var app = express()
+
+app.get('/', function(req, res) {
+  res.send('Hello world from ' + hostname + '! Your app is up and running!\n')
+  //res.send('Welcome to ' + hostname + '! Your app is up and running!\n'
+  //res.send(process.env.MESSAGE + '\n' sin hardcodeo
+})
+app.listen(8080, function() {
+  console.log('Sample app is listening on port 8080.')
+})
